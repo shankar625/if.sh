498f3dea806e6ef9d351bc94915e53ae41f3f91e
shankar625 <suryashankar625@gmail.com>
2019-03-28 21:51:33 +0530

    fist

--- if.sh
+++ if.sh
@@ -3,6 +3,9 @@ int1=$1
 int2=$2
 if [ $int1 -eq $int2 ];then
 echo "values are equal"
-else 
-echo "values are not equal"
+elif [ $int1 -gt $int2 ];then
+echo "values are grater then" 
+else
+echo "values are not grater "
+
 fi
